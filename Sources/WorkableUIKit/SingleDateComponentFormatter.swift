e25ae7136423d349ee84c02c90caf27e91f8e2c1
Ben Spratling IV <bspratling@mac.com>
2022-12-14 22:41:37 -0600

    Fix SingleDateComponentFormatter
    Round up when value is less than smallest unit.
    Round to larger unit when rounded unit does not fit evenly into larger units.

--- Sources/WorkableUIKit/SingleDateComponentFormatter.swift
+++ Sources/WorkableUIKit/SingleDateComponentFormatter.swift
@@ -32,15 +32,35 @@ public struct SingleDateComponentFormatter {
 		return residualFormatter.string(from: componentsFloor)
 	}
 	
+	
 	///generates not only the floor-rounded string, but also a time interval until it should change
 	public func stringAndIntervalToChange(from startDate:Date, to endDate:Date)->(String, TimeInterval)? {
 		let components = calendar.dateComponents(allowedUnits, from: startDate, to: endDate)
 		let (componentsFloor, roundedUnit) = components.floorAndRoundedUnit(allowedUnits: [Calendar.Component](allowedUnits))
 		//to make the calendar give us the ceil, we'll add 1 of the largest non-zero unit to the components, and floor that
 		guard let actualTimeOfFloor = calendar.date(byAdding: componentsFloor, to: startDate) else { return nil }
+		//but this unit may not fit integrally into the larger
+		let allUnits = [Calendar.Component].inDescendingMagnitudeOrder.filter({ allowedUnits.contains($0) })
 		guard let oneMoreUnitAfterFloor = calendar.date(byAdding: roundedUnit, value: 1, to: actualTimeOfFloor) else { return nil }
 		guard let string = residualFormatter.string(from: componentsFloor) else { return nil }
-		return (string, oneMoreUnitAfterFloor.timeIntervalSince(endDate))
+		func returnByAddingOne()->(String, TimeInterval)? {
+			//this code assumes units fit integrally into the next larger unit, which is fine until weeks into months
+			return (string, oneMoreUnitAfterFloor.timeIntervalSince(endDate))
+		}
+		let allComponentsAtFloor = calendar.dateComponents(allowedUnits, from:startDate, to: actualTimeOfFloor)
+		let oneUnitLargerComponents = calendar.dateComponents(allowedUnits, from:startDate, to: oneMoreUnitAfterFloor)
+		let largerUnits = allUnits.unitsLargerThan(roundedUnit)
+		for aLargerUnit in largerUnits.reversed() {
+			if oneUnitLargerComponents.value(for: aLargerUnit) != allComponentsAtFloor.value(for: aLargerUnit) {
+				//the next unit will be reached after actualTimeOfFloor but at or before oneMoreUnitAfterFloor
+				let smallerComponentsToIgnore = allowedUnits.filter({ !largerUnits.contains($0) }).union([roundedUnit])
+				let biggerFloor:DateComponents = oneUnitLargerComponents.settingComponentsToZero([Calendar.Component](smallerComponentsToIgnore))
+				guard let timeAtBiggerFloor = calendar.date(byAdding: biggerFloor, to: startDate) else { continue }
+				return (string, timeAtBiggerFloor.timeIntervalSince(endDate))
+			}
+		}
+		//none of the larger unit were different, we can use the oneMoreUnitAfterFloor as the next time just fine
+		return returnByAddingOne()
 	}
 	
 	public var calendar:Calendar
@@ -111,29 +131,32 @@ extension DateComponents {
 	func floor(allowedUnits:[Calendar.Component] = .inDescendingMagnitudeOrder)->DateComponents {
 		let units = [Calendar.Component].inDescendingMagnitudeOrder.filter({ allowedUnits.contains($0) })
 		for (componentIndex, component) in units.enumerated() {
-			let valueOrNil = self.value(for: component)
+			let valueOrNil = value(for: component)
 			guard let value = valueOrNil else { continue }
 			if value > 0 {
 				let remainingComponents = [Calendar.Component](units[(componentIndex+1)...])
+				//if one of the allowed unit is non-zero, set the rest to zero
 				return settingComponentsToZero(remainingComponents)
 			}
 		}
-		return self
+		//if none of the allowed units was non-zero, then we need to
+		return ceilIfNonZero(allowedUnits:allowedUnits)
 	}
 	
 	func floorAndRoundedUnit(allowedUnits:[Calendar.Component] = .inDescendingMagnitudeOrder)->(DateComponents, Calendar.Component) {
 		let units = [Calendar.Component].inDescendingMagnitudeOrder.filter({ allowedUnits.contains($0) })
+		
 		for (componentIndex, component) in units.enumerated() {
-			let valueOrNil = self.value(for: component)
+			let valueOrNil = value(for: component)
 			guard let value = valueOrNil else { continue }
 			if value > 0 {
 				let remainingComponents = [Calendar.Component](units[(componentIndex+1)...])
-				//TODO: remove disallowed units completely
-				
-				return (settingComponentsToZero(remainingComponents), component)
+				return (settingComponentsToZero(remainingComponents)
+					, component)
 			}
 		}
-		return (self, .nanosecond)
+		let (ceil, ceilUnitOrNil) = ceilIfNonZeroAndUnit(allowedUnits:allowedUnits)
+		return (ceil, ceilUnitOrNil ?? .nanosecond)
 	}
 	
 	func settingComponentsToZero(_ components:[Calendar.Component])->DateComponents {
@@ -142,6 +165,42 @@ extension DateComponents {
 			newValue.setValue(nil, for: component)
 		}
 		return newValue
+	}
+	
+	//if this date components is non-zero, but the value is below the allowed units, then round it up to the smallest unit
+	func ceilIfNonZero(allowedUnits:[Calendar.Component] = .inDescendingMagnitudeOrder)->DateComponents {
+		return ceilIfNonZeroAndUnit(allowedUnits:allowedUnits).0
+	}
+	
+	//if this date components is non-zero, but the value is below the allowed units, then round it up to the smallest unit and report which unit that was
+	func ceilIfNonZeroAndUnit(allowedUnits:[Calendar.Component] = .inDescendingMagnitudeOrder)->(DateComponents, Calendar.Component?) {
+		//if allowed units are non-zero, return self
+		for unit in allowedUnits {
+			if let unitValue = value(for: unit) {
+				if unitValue > 0, unitValue < Int64.max {
+					return (self, nil)
+				}
+			}
+		}
+		
+		//in self, if units smaller than the smallest unit in allowedUnits
+		guard let lastIndex = allowedUnits
+			.compactMap({ [Calendar.Component].inDescendingMagnitudeOrder.lastIndex(of:$0) })
+			.sorted(by: >)
+			.first
+			else { return (self, nil) }
+		
+		let smallestAllowedUnit = [Calendar.Component].inDescendingMagnitudeOrder[lastIndex]
+		var newComponents = DateComponents(calendar: calendar, timeZone: timeZone)
+		if let smallestValue = value(for: smallestAllowedUnit)
+			, smallestValue > 0
+			, smallestValue < Int64.max {
+			//we already have a non-zero smallest unit
+		}
+		else {
+			newComponents.setValue(1, for: smallestAllowedUnit)
+		}
+		return (newComponents, smallestAllowedUnit)
 	}
 	
 }
@@ -166,6 +225,21 @@ extension Array where Element == Calendar.Component {
 		.nanosecond,
 	]
 	
+	//assuming self is ordered in descending order,
+	//returns nil if there are no larger units
+	func nextLargerAllowedUnit(than unit:Calendar.Component)->Calendar.Component? {
+		guard let index = firstIndex(of: unit)
+			,index > 0
+			else { return nil }
+		return self[index - 1]
+	}
+	
+	
+	func unitsLargerThan(_ aUnit:Calendar.Component)->[Calendar.Component] {
+		guard let index = firstIndex(of: aUnit)
+			,index > 0
+			else { return [] }
+		return [Calendar.Component](self[0..<index])
+	}
 	
 }
-
